cdc655c24f8d9a771d6e67776b6d86b108eb545b
John Taylor <johntaylor@gmail.com>
2018-11-15 11:57:33 -0500

    initial commit

---
+++ vincenty_test.go
@@ -0,0 +1,48 @@
+package vincenty
+
+import "testing"
+
+func TestVincentyDistance(t *testing.T) {
+	// distances computed from: http://www.5thandpenn.com/GeoMaps/GMapsExamples/distanceComplete2.html
+	// distance NY-SD: 3915340.577 m
+	// distance SD-EP: 1011300.217 m
+	// distance EP-SL: 1663833.491 m
+	// distance SL-NY: 1406519.972 m
+	var newYork = Coord{40.7128,74.0060}
+	var sanDiego = Coord{32.7157,117.1611}
+	var elPaso = Coord{31.7619, 106.4850}
+	var stLouis = Coord{38.6270,90.1994}
+
+	var miles, km float64
+	var ok bool
+
+	miles, km, ok = VincentyDistance(newYork,sanDiego)
+	if int(miles) != 2432 || int64(km) != 3915340576 || ok == false {
+		t.Errorf("Computed values: %v %10f %v\n", miles,km,ok)
+		t.Errorf("In correct computation between New York and San Diego: %v %v %v\n", int(miles), int64(km), ok)
+	}
+
+	miles, km, ok = VincentyDistance(sanDiego,elPaso)
+	if int(miles) != 628 || int64(km) != 1011300216 || ok == false {
+		t.Errorf("Computed values: %v %10f %v\n", miles,km,ok)
+		t.Errorf("In correct computation between San Diego and El Paso: %v %v %v\n", int(miles), int64(km), ok)
+	}
+
+	miles, km, ok = VincentyDistance(elPaso,stLouis)
+	if int(miles) != 1033 || int(km) != 1663833491 || ok == false {
+		t.Errorf("Computed values: %v %10f %v\n", miles,km,ok)
+		t.Errorf("In correct computation between El Paso and St. Louis: %v %v %v\n", int(miles), int64(km), ok)
+	}
+
+	miles, km, ok = VincentyDistance(stLouis,newYork)
+	if int(miles) != 873 || int(km) != 1406519971 || ok == false {
+		t.Errorf("Computed values: %v %10f %v\n", miles,km,ok)
+		t.Errorf("In correct computation between St. Louis and New York: %v %v %v\n", int(miles), int64(km), ok)
+	}
+
+	miles, km, ok = VincentyDistance(newYork,newYork)
+	if int(miles) != 0 || int(km) !=  0 || ok == false {
+		t.Errorf("Computed values: %v %10f %v\n", miles,km,ok)
+		t.Errorf("In correct computation between New York and New York: %v %v %v\n", int(miles), int64(km), ok)
+	}
+}
